9d19c0ee60db34c559a688e97c2790d712b9b98c
Fexelitche <gustavbw@gmail.com>
2022-12-02 00:18:00 +0100

    Less lines is better. Right?

--- AoC22/src/main/java/first/Solver2.java
+++ AoC22/src/main/java/first/Solver2.java
@@ -3,6 +3,9 @@ package first;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Solver2 {
 
@@ -12,39 +15,31 @@ public class Solver2 {
 
     public static final String filepath = "src/main/java/first/input.txt";
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException, NumberFormatException
     {
+        long timeA = System.currentTimeMillis();
         System.out.println(new Solver2().getCalorieCountOfTopThree(filepath));
+        System.out.println("Solve time: " + (System.currentTimeMillis() - timeA) + "ms");
     }
 
-    public int getCalorieCountOfTopThree(String filepath)
+    public int getCalorieCountOfTopThree(String filepath) throws IOException, NumberFormatException
     {
         int[] topThree = new int[3];
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
 
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(filepath));
+        String currentLine = "";
+        int currentElfCalCount = 0;
 
-            String currentLine = "";
-            int currentElfCalCount = 0;
-
-            while((currentLine = br.readLine()) != null){
-                if(!currentLine.equals("")){
-
-                    currentElfCalCount += Integer.parseInt(currentLine);
-
-                }else{  //New elf encountered in file. Begin new count.
-
-                    replaceIndexIfLessThan(currentElfCalCount,topThree);
-                    currentElfCalCount = 0;
-                }
+        while((currentLine = bufferedReader.readLine()) != null){
+            if(!currentLine.equals("")){
+                currentElfCalCount += Integer.parseInt(currentLine);
+            }else{  //New elf encountered in file. Begin new count.
+                replaceIndexIfLessThan(currentElfCalCount,topThree);
+                currentElfCalCount = 0;
             }
-
-            br.close();
-        }catch (IOException | NumberFormatException | NullPointerException e){
-            e.printStackTrace();
         }
-
-        return topThree[0]+topThree[1]+topThree[2];
+        bufferedReader.close();
+        return topThree[0] + topThree[1] + topThree[2];
     }
 
     public void replaceIndexIfLessThan(int a, int[] b)
@@ -56,6 +51,4 @@ public class Solver2 {
             }
         }
     }
-
-
 }
