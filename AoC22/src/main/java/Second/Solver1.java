bcaa357d4dabbe3a6aa49b139a112677b2adee02
Fexelitche <gustavbw@gmail.com>
2022-12-02 14:08:25 +0100

    Branchless solution for task 2 today :D

---
+++ AoC22/src/main/java/Second/Solver1.java
@@ -0,0 +1,55 @@
+package Second;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solver1 {
+
+    public static final String filepath = "src/main/java/second/input.txt";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Score when following guide: " + new Solver1().calculateScore(filepath));
+    }
+
+    private static Map<String, Integer> valueOfChoiceMap = new HashMap<>(
+            Map.of("A", 1, "B", 2, "C", 3));
+    private static Map<String, String> whoBeatsWhoMap = new HashMap<>(Map.of(
+            "A", "C", //Rock beats Scissors
+            "B", "A", //Paper beats Rock
+            "C", "B" //Scissors beats Paper
+    ));
+    private static Map<String, String> mapYourChoiceToABC = new HashMap<>(Map.of(
+            "X","A",
+            "Y","B",
+            "Z","C"
+    ));
+
+    public int calculateScore(String filepath) throws IOException, NumberFormatException
+    {
+        //Enemy Choice | Your Choice \n
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
+        String currentLine;
+        String[] playerChoices = null;
+        int scoreSum = 0;
+
+        while((currentLine = bufferedReader.readLine()) != null){
+            playerChoices = currentLine.split(" ");
+            scoreSum += calculateRoundResult(playerChoices[0], mapYourChoiceToABC.get(playerChoices[1]));
+        }
+
+        return scoreSum;
+    }
+    public int calculateRoundResult(String enemyChoice, String yourChoice)
+    {
+        int outcome = 0; //0 on loss, 3 on draw, 6 on win
+        if(whoBeatsWhoMap.get(yourChoice).equals(enemyChoice)){
+            outcome = 6;
+        }else if(enemyChoice.equals(yourChoice)){
+            outcome = 3;
+        }
+        return valueOfChoiceMap.get(yourChoice) + outcome;
+    }
+}
---
+++ AoC22/src/main/java/Second/Solver2.java
@@ -0,0 +1,63 @@
+package Second;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solver2 {
+
+    public static final String filepath = "src/main/java/second/input.txt";
+
+    @FunctionalInterface
+    public static interface Func{
+        int evaluate(String enemyChoice);
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Score when following guide: " + new Solver2().calculateScore(filepath));
+    }
+
+    private static final Map<String, Integer> valueOfChoiceMap = new HashMap<>(
+            Map.of("A", 1, "B", 2, "C", 3));
+    private static final Map<String, String> whoBeatsWhoMap = new HashMap<>(Map.of(
+            "A", "C", //Rock beats Scissors
+            "B", "A", //Paper beats Rock
+            "C", "B" //Scissors beats Paper
+    ));
+    private static final Map<String, String> whoLoosesToWhoMap = new HashMap<>(Map.of(
+            "C","A",
+            "A", "B",
+            "B","C"
+    ));
+    //You need to win
+    //You need to loose
+    private static Map<String, Func> approachMap = new HashMap<>(Map.of(
+            "X", enemyChoice -> valueOfChoiceMap.get(whoBeatsWhoMap.get(enemyChoice)),
+            // You need to draw the match
+            "Y", enemyChoice ->  3 + valueOfChoiceMap.get(enemyChoice),
+            "Z", enemyChoice -> 6 + valueOfChoiceMap.get(whoLoosesToWhoMap.get(enemyChoice))
+    ));
+
+    public int calculateScore(String filepath) throws IOException, NumberFormatException
+    {
+        //Enemy Choice | Your Choice \n
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
+        String currentLine;
+        String[] playerChoices = null;
+        int scoreSum = 0;
+
+        while((currentLine = bufferedReader.readLine()) != null){
+            playerChoices = currentLine.split(" ");
+            scoreSum += calculateRoundResult(playerChoices[0], playerChoices[1]);
+        }
+
+        return scoreSum;
+    }
+    public int calculateRoundResult(String enemyChoice, String approach)
+    {
+        return approachMap.get(approach).evaluate(enemyChoice);
+    }
+
+}
