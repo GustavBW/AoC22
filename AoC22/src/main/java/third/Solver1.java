afa218e55fb0c3d510b295ddfc2fc5f6af0e5061
GustavBW <37631401+GustavBW@users.noreply.github.com>
2022-12-03 14:11:11 +0100

    Solution first task

--- AoC22/src/main/java/third/Solver1.java
+++ AoC22/src/main/java/third/Solver1.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 public class Solver1 {
 
     public static final String filepath = "src/main/java/third/input.txt";
+    public static final String priorities = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     /*
     Find the item type that appears in both compartments of each rucksack.
@@ -16,58 +17,46 @@ public class Solver1 {
      */
 
     public static void main(String[] args) throws IOException {
+        long timeA = System.currentTimeMillis();
         System.out.println("Sum of priorities of items in both compartments are: " + getSumOfPriorities());
+        System.out.println("Solve time: " + (System.currentTimeMillis() - timeA) + "ms");
     }
 
-    public static int getSumOfPriorities() throws IOException {
+    public static int getSumOfPriorities() throws IOException
+    {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
-
         String currentLine;
         int prioritySum = 0;
 
         while((currentLine = bufferedReader.readLine()) != null){
             String[] compartments = getCompartments(currentLine);
-            byte[] inBoth = findDublicate(compartments);
+            char inBoth = findDuplicate(compartments);
             prioritySum += getPriority(inBoth);
         }
-
         return prioritySum;
     }
 
-    private static byte[] findDublicate(String[] compartments) {
-        for(int i = 0; i < compartments[0].length(); i++){
-            for(int j = 0; j < compartments[1].length(); j++){
-                if((compartments[0].charAt(i)+"").equalsIgnoreCase(compartments[1].charAt(j)+"")){
-                    return new byte[]{
-                            (byte) compartments[0].charAt(i),
-                            (byte) compartments[1].charAt(j)
-                    };
-                }
+    private static char findDuplicate(String[] compartments)
+    {
+        for(char b : compartments[0].toCharArray()){
+            if(compartments[1].indexOf(b) != -1){
+                return b;
             }
         }
-        return new byte[2];
+        return 'a';
     }
 
     public static String[] getCompartments(String s)
     {
         String[] compartments = new String[2];
-        int middle = (int) Math.floor(s.length() / 2.0);
+        int middle = (int) (s.length() / 2.0);
         compartments[0] = s.substring(0,middle);
         compartments[1] = s.substring(middle);
         return compartments;
     }
 
-    public static int getPriority(byte[] bArr)
+    public static int getPriority(char c)
     {
-        int sum = 0;
-        for(byte b : bArr){
-            if(b < 96){
-                sum += b - 64;
-            }else{
-                sum += b - 96;
-            }
-        }
-        System.out.println("equals: " + (char) bArr[1] + " " + (char) bArr[0]);
-        return sum;
+        return priorities.indexOf(c) + 1;
     }
 }
