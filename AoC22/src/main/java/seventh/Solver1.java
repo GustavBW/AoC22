c3c39a2b5e888da0baa55632b345f08d9ca6ae6d
GustavBW <gustavbw@gmail.com>
2022-12-07 09:13:21 +0100

    Solutions for today (unoptimized)

--- AoC22/src/main/java/seventh/Solver1.java
+++ AoC22/src/main/java/seventh/Solver1.java
@@ -11,34 +11,63 @@ public class Solver1 {
 
     public static void main(String[] args) throws IOException
     {
-        /*long totalMS = 0;
+        long totalMS = 0;
         long timeA = System.nanoTime();
-        for(int i = 0; i < 1_000_000; i++){
-            getSizeOfDirsAbove100kSize();
+        for(int i = 0; i < 1_000; i++){
+            getSumOfSizesOfDirsOfAtMost100kSize();
         }
-        totalMS += (System.nanoTime() - timeA) / 1_000_000;
-         */
-        System.out.println("Character count position of package start marker: " + getSizeOfDirsAbove100kSize());
-        //System.out.println("Solve time: " + totalMS + "ns");
+        totalMS += (System.nanoTime() - timeA) / 1_000;
+
+        //44974370 is too high
+        //1274107 is too low
+        System.out.println("Sum of dir sizes for dirs above threshold: " + getSumOfSizesOfDirsOfAtMost100kSize());
+        System.out.println("Solve time: " + totalMS + "ns");
     }
 
-    private static record ElfFile(int size, String name){};
-    private static record ElfDir(String name, ElfDir parent, List<ElfDir> subDirs, List<ElfFile> files){};
+    private record ElfFile(int size, String name){}
+    public static class ElfDir{
+        public String name;
+        public ElfDir parent;
+        public List<ElfDir> subDirs;
+        public List<ElfFile> files;
+        public int size;
+        public ElfDir(String name, ElfDir parent, List<ElfDir> subDirs, List<ElfFile> files, int size)
+        {
+            this.name = name;
+            this.parent = parent;
+            this.subDirs = subDirs;
+            this.files = files;
+            this.size = size;
+        }
+        @Override
+        public String toString(){
+            return "dir: " + name + " " + size + "bytes" + " | " + "\n\t" + subDirs.toString();
+        }
+    }
 
-    public static int getSizeOfDirsAbove100kSize() throws IOException
+    public static int getSumOfSizesOfDirsOfAtMost100kSize() throws IOException
     {
         ElfDir rootDir = loadFileTree();
+        int threshold = 100_000;
 
-        System.out.println(rootDir);
+        int sumOfDirsAboveThreshold = 0;
 
-        return 1;
+        Queue<ElfDir> queue = new ArrayDeque<>(rootDir.subDirs);
+        ElfDir currentDir;
+
+        while((currentDir = queue.poll()) != null){
+            sumOfDirsAboveThreshold += currentDir.size <= threshold ? currentDir.size : 0;
+            queue.addAll(currentDir.subDirs);
+        }
+
+        return sumOfDirsAboveThreshold;
     }
 
     public static ElfDir loadFileTree() throws IOException
     {
         BufferedReader reader = new BufferedReader(new FileReader(filepath));
-        ElfDir rootDir = new ElfDir("/",null, new ArrayList<>(),new ArrayList<>());
-        String currentLine = "";
+        ElfDir rootDir = new ElfDir("/",null, new ArrayList<>(),new ArrayList<>(),0);
+        String currentLine;
         ElfDir currentWorkingDirectory = rootDir;
 
         while((currentLine = reader.readLine()) != null){
@@ -46,7 +75,7 @@ public class Solver1 {
 
             if(split.length == 3){
                 if(split[2].equals("..")){
-                    currentWorkingDirectory = currentWorkingDirectory.parent() == null ? rootDir : currentWorkingDirectory.parent();
+                    currentWorkingDirectory = currentWorkingDirectory.parent == null ? rootDir : currentWorkingDirectory.parent;
                     continue;
                 }
                 currentWorkingDirectory = traverseAndFindDir(currentWorkingDirectory,split[2]);
@@ -62,27 +91,41 @@ public class Solver1 {
     private static void addDirOrFile(ElfDir currentWorkingDirectory, String[] split) {
         if(!split[0].equals("$")){
             if(split[0].equals("dir")){
-                currentWorkingDirectory.subDirs().add(
-                        new ElfDir(split[1], currentWorkingDirectory,new ArrayList<>(),new ArrayList<>())
+                currentWorkingDirectory.subDirs.add(
+                        new ElfDir(
+                                split[1], currentWorkingDirectory,new ArrayList<>(),new ArrayList<>(), 0
+                        )
                 );
                 return;
             }
-            currentWorkingDirectory.files().add(
-                    new ElfFile(Integer.parseInt(split[0]), split[1])
-            );
+            ElfFile newFile = new ElfFile(Integer.parseInt(split[0]), split[1]);
+            currentWorkingDirectory.files.add(newFile);
+            currentWorkingDirectory.size += newFile.size();
+            propegateSizeIncrease(currentWorkingDirectory, newFile.size());
+        }
+    }
+
+    public static void propegateSizeIncrease(ElfDir subDir, int sizeIncrease)
+    {
+        ElfDir parent;
+        ElfDir current = subDir;
+        while((parent = current.parent) != null){
+            parent.size += sizeIncrease;
+            current = parent;
         }
     }
 
     public static ElfDir traverseAndFindDir(ElfDir rootOfSearch, String name)
-    {   //TODO: Do a breadth first search instead
-        if(rootOfSearch.name().equals(name)){
-            return rootOfSearch;
-        }
-        for(ElfDir dir : rootOfSearch.subDirs){
-            if(dir.name().equals(name)){
-                return dir;
+    {
+        Queue<ElfDir> dirsToSearch = new ArrayDeque<>(rootOfSearch.subDirs);
+        dirsToSearch.add(rootOfSearch);
+
+        ElfDir currentDir;
+        while((currentDir = dirsToSearch.poll()) != null){
+            if(currentDir.name.equals(name)){
+                return currentDir;
             }
-            traverseAndFindDir(dir,name);
+            dirsToSearch.addAll(currentDir.subDirs);
         }
         return rootOfSearch;
     }
@@ -90,7 +133,7 @@ public class Solver1 {
     public static int sizeOf(ElfDir dir)
     {
         int sum = 0;
-        for(ElfFile file : dir.files()){
+        for(ElfFile file : dir.files){
             sum += file.size();
         }
         return sum;
---
+++ AoC22/src/main/java/seventh/Sovler2.java
@@ -0,0 +1,47 @@
+package seventh;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Sovler2 {
+
+    public static void main(String[] args) throws IOException
+    {
+        long totalMS = 0;
+        long timeA = System.nanoTime();
+        for(int i = 0; i < 1_000; i++){
+            getSizeOfSmallestViableDir();
+        }
+        totalMS += (System.nanoTime() - timeA) / 1_000;
+
+        //44974370 is too high
+        //1274107 is too low
+        System.out.println("Size of smallest dir above delete threshold: " + getSizeOfSmallestViableDir());
+        System.out.println("Solve time: " + totalMS + "ns");
+    }
+
+    public static int getSizeOfSmallestViableDir() throws IOException
+    {
+        seventh.Solver1.ElfDir rootDir = seventh.Solver1.loadFileTree();
+
+        int totalCapacity = 70_000_000; //70MB
+        int freeSpace = totalCapacity - rootDir.size;
+        int updateSize = 30_000_000; //30MB
+        int deleteThreshold = updateSize - freeSpace;
+
+        Queue<Solver1.ElfDir> queue = new ArrayDeque<>(rootDir.subDirs);
+        Solver1.ElfDir currentDir;
+        List<Solver1.ElfDir> dirsThatQualifies = new ArrayList<>();
+
+        while((currentDir = queue.poll()) != null){
+            if(currentDir.size >= deleteThreshold){
+                dirsThatQualifies.add(currentDir);
+            }
+            queue.addAll(currentDir.subDirs);
+        }
+
+        dirsThatQualifies.sort(Comparator.comparingInt(o -> o.size));
+        return dirsThatQualifies.get(0).size;
+    }
+
+}
